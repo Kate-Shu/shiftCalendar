82b768c84fea6f66bd9e65b9406e3a9895d0c682
Kate-Shu <k.onufrichuk@gmail.com>
2025-01-15 11:42:10 +0100

    feat: setup project with base dependencies

---
+++ rsbuild.config.ts
@@ -0,0 +1,40 @@
+import {defineConfig} from '@rsbuild/core';
+import {pluginReact} from '@rsbuild/plugin-react';
+import {pluginSvgr} from '@rsbuild/plugin-svgr';
+import {pluginTypeCheck} from '@rsbuild/plugin-type-check';
+
+export default defineConfig({
+  dev: {
+    lazyCompilation: true,
+  },
+  html: {
+    template: 'public/index.html',
+  },
+  // source: {
+  // transformImport: [
+  // {
+  //   libraryName: '@mui/icons-material',
+  //   customName: '@mui/icons-material/{{ member }}',
+  // },
+  //     {
+  //       libraryName: 'lodash',
+  //       customName: 'lodash/{{ member }}',
+  //     },
+  //   ],
+  // },
+  output: {
+    polyfill: 'usage',
+    cleanDistPath: true,
+    legalComments: 'none',
+    assetPrefix: 'auto',
+  },
+  plugins: [
+    pluginReact(),
+    pluginSvgr({svgrOptions: {ref: true}}),
+    pluginTypeCheck(),
+  ],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+});
